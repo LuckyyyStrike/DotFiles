b063317c6adf72ab4e78484ade3c2203313b4359
LuckyyyStrike <ingomohrmann@posteo.de>
2026-03-06 23:15:41 +0100

    Added hotkey for opening my todo.txt

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,23 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Hotkey for opening my Todolist
+vim.keymap.set("n", "<leader>wt", ":e ~/vimwiki/todo.txt<CR>", { desc = "Open todo.txt" })
+
 vim.keymap.set("n", "<leader>rp", function()
-  local file = vim.fn.expand("%:p")
+    local file = vim.fn.expand("%:p")
 
-  -- Ghostty braucht das Kommando direkt nach dem -e ohne Umwege.
-  -- Wir nutzen Doppelzitate für den hyprctl-String und einfache für das sh-Kommando.
-  local cmd = "hyprctl dispatch exec \"[float] ghostty -e sh -c 'python3 " .. file .. "; echo; read'\""
+    -- Ghostty braucht das Kommando direkt nach dem -e ohne Umwege.
+    -- Wir nutzen Doppelzitate für den hyprctl-String und einfache für das sh-Kommando.
+    local cmd = "hyprctl dispatch exec \"[float] ghostty -e sh -c 'python3 " .. file .. "; echo; read'\""
 
-  vim.fn.jobstart(cmd)
+    vim.fn.jobstart(cmd)
 end, { desc = "Run Python in Ghostty Float" })
 
 
 vim.keymap.set("n", "<leader>dd", function()
-  require("neotest").run.run({ strategy = "dap" }) -- Test unter Cursor debuggen
+    require("neotest").run.run({ strategy = "dap" }) -- Test unter Cursor debuggen
 end, { desc = "Debug test under cursor" })
 
 vim.keymap.set("n", "<leader>dD", function()
-  require("neotest").run.run({ strategy = "dap", suite = true }) -- ganze Datei debuggen
+    require("neotest").run.run({ strategy = "dap", suite = true }) -- ganze Datei debuggen
 end, { desc = "Debug all tests in file" })
 
 local map = vim.keymap.set
@@ -35,5 +39,5 @@ map("n", "<leader>db", function() require 'dap'.toggle_breakpoint() end, { desc 
 map("n", "<leader>dr", function() require 'dap'.repl.open() end, { desc = "Open DAP REPL" })
 
 map("n", "<leader>ds", function()
-  require('dap').run({ type = 'python', request = 'launch', program = vim.fn.expand("%") })
+    require('dap').run({ type = 'python', request = 'launch', program = vim.fn.expand("%") })
 end, { desc = "Debug current file" })
